258a40755c7a280ceedfae0890a2bd0b9a469750
Raphaël Delhome <raphael.delhome@oslandia.com>
2020-12-06 10:16:21 +0100

    2020 day06

---
+++ 2020/day06/part02.py
@@ -0,0 +1,34 @@
+INPUT_FILEPATH = "2020/day06/input"
+TEST_INPUT_FILEPATH = "2020/day06/test_input"
+
+ALPHABET = "".join(map(chr, range(ord("a"), ord("a") + 26)))
+
+
+def read_input(filepath):
+    groups = []
+    with open(filepath, "r") as fobj:
+        new_group = []
+        for line in fobj:
+            if line == "\n":
+                groups.append(new_group)
+                new_group = []
+            else:
+                new_group.append(line.strip())
+        groups.append(new_group)
+    return groups
+
+
+def sum_count_all(group, alphabet=ALPHABET):
+    return sum(all(letter in item for item in group) for letter in alphabet)
+
+
+if __name__ == "__main__":
+
+    test_groups = read_input(TEST_INPUT_FILEPATH)
+    test_count_sum = sum(sum_count_all(item) for item in test_groups)
+    print("Sum of 'yes' answer counts (test set): {}".format(test_count_sum))
+    assert test_count_sum == 6
+
+    groups = read_input(INPUT_FILEPATH)
+    count_sum = sum(sum_count_all(item) for item in groups)
+    print("Sum of 'yes' answer counts: {}".format(count_sum))
