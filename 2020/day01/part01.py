e4d0e4fe5fef2a6dc58e63826fcea8c945238d72
Raphaël Delhome <raphael.delhome@oslandia.com>
2020-12-07 19:10:45 +0100

    2020 day01: slight cleanings

--- 2020/day01/part01.py
+++ 2020/day01/part01.py
@@ -9,6 +9,4 @@ if __name__ == "__main__":
         for l1 in lines:
             for l2 in lines:
                 if int(l1) + int(l2) == 2020:
-                    print(l1, l2)
                     print("l1 * l2 = ", int(l1) * int(l2))
-                    break
--- 2020/day01/part02.py
+++ 2020/day01/part02.py
@@ -6,7 +6,7 @@ def find_2020_terms(items):
         for l2 in items:
             for l3 in items:
                 if int(l1) + int(l2) + int(l3) == 2020:
-                    return [l1, l2, l3, int(l1) * int(l2) * int(l3)]
+                    return int(l1) * int(l2) * int(l3)
     
 
 if __name__ == "__main__":
@@ -14,7 +14,6 @@ if __name__ == "__main__":
     with open("input", "r") as fobj:
 
         lines = fobj.readlines()
-        l1, l2, l3, mult = find_2020_terms(lines)
-        print(l1, l2, l3)
+        mult = find_2020_terms(lines)
         print("l1 * l2 * l3 = ", mult)
         
